6e686e287f3e319e701371adbc39b4d2ed1b9202
Alex Natashkin <natashkinsasha@gmail.com>
2017-11-11 19:26:31 +0300

    v 2.0.0

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var config = require('config');
+var JSON5 = require('json5-utils')
 
 module.exports = function () {
     var t = config;
@@ -13,35 +14,64 @@ function parse(config) {
     var newConfig = Object.assign({}, config);
     newConfig = parseNumber(newConfig);
     newConfig = parseBoolean(newConfig);
+    newConfig = parseJSON(newConfig);
     return newConfig;
 }
 
+
 function parseBoolean(obj) {
-    return Object.keys(obj).reduce(function (result, key) {
-        var value = obj[key];
-        var parsedValue = value === 'true' ? true : value === 'false' ? false : value;
-        if (typeof parsedValue === 'boolean') {
-            result[key] = parsedValue;
-        } else if (value.constructor === Object) {
-            result[key] = parseBoolean(value);
-        } else {
-            result[key] = value;
-        }
-        return result;
-    }, {});
+    return Object
+        .keys(obj)
+        .reduce(function (result, key) {
+            var value = obj[key];
+            var parsedValue = value === 'true' ? true : value === 'false' ? false : value;
+            if (typeof parsedValue === 'boolean') {
+                result[key] = parsedValue;
+            } else if (value.constructor === Object) {
+                result[key] = parseBoolean(value);
+            } else {
+                result[key] = value;
+            }
+            return result;
+        }, {});
 }
 
 function parseNumber(obj) {
-    return Object.keys(obj).reduce(function (result, key) {
-        var value = obj[key];
-        var parsedValue = +value;
-        if (typeof value === 'string' && !isNaN(parsedValue)) {
-            result[key] = parsedValue;
-        } else if (value.constructor === Object) {
-            result[key] = parseNumber(value);
-        } else {
-            result[key] = value;
-        }
-        return result;
-    }, {});
+    return Object
+        .keys(obj)
+        .reduce(function (result, key) {
+            var value = obj[key];
+            var parsedValue = +value;
+            if (typeof value === 'string' && !isNaN(parsedValue)) {
+                result[key] = parsedValue;
+            } else if (value.constructor === Object) {
+                result[key] = parseNumber(value);
+            } else {
+                result[key] = value;
+            }
+            return result;
+        }, {});
+}
+
+function parseJSON(obj) {
+    return Object
+        .keys(obj)
+        .reduce(function (result, key) {
+            var value = obj[key];
+            if (isJsonString(value)) {
+                result[key] = JSON5.parse(value);
+            } else{
+                result[key] = value;
+            }
+            return result;
+        }, {});
+}
+
+function isJsonString(str) {
+    try {
+        JSON5.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
 }
